5b509b8f152b387ccbf6930740b1cd13d6629b01
“redxblue” <sharonjob410@gmail.com>
2023-05-01 22:15:08 +0530

    core smart contract functions added

--- contracts/PropertyNft.sol
+++ contracts/PropertyNft.sol
@@ -6,21 +6,63 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract PropertyNft is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-
+    // modifier onlyPropertyOwner(_tokenId){
+    //     require(msg.sender== ownerOf(_tokenId),"function only callable by owner of this property");
+    //     _;
+    // }
+    struct property{
+        uint256 securityDeposit; //is the value getting stored as gwei here??
+        uint256 rentAmount; //testing rquired👆
+        }
+    mapping(uint256=>address)public tenentOf;
+   // property public propInfo;
+    mapping(uint256=>property) public propertyInfo; 
     Counters.Counter private _tokenIdCounter;
     constructor() ERC721("PropertyNft", "PNT") {
         // Start token ID at 1. By default is starts at 0.
         // _tokenIdCounter.increment();
     }
-    function mint(address _to, string calldata _uri) external returns (uint256)  {
+    function mint(address _to, string calldata _uri,uint256 _securityDeposit,
+    uint256 _rentAmount) external returns (uint256)  {
             uint256 _tokenId=_tokenIdCounter.current();
             _tokenIdCounter.increment();
+            propertyInfo[_tokenId].securityDeposit=_securityDeposit*10**18;
+            propertyInfo[_tokenId].rentAmount=_rentAmount*10**18; //representing as eth in wei 1eth= 10^18 wei 
             _mint(_to, _tokenId);
             _setTokenURI(_tokenId, _uri);
             return _tokenId; 
     }
+    function rent(uint256 _tokenId) external payable {
+        address payable propertyOwner = payable(ownerOf(_tokenId));
+		uint256 amount = msg.value; // get the amount of ether in the contract
+        uint256 requiredAmount=propertyInfo[_tokenId].securityDeposit + propertyInfo[_tokenId].rentAmount; //gwei is converted to wei here i guess
+		require(amount >= requiredAmount, 'Sent an amount equal to the Security deposit and Rent');
+        //address payable propertyOwner= payable (ownerOf(_tokenId));
+        (bool success, ) = address(this).call{value: (amount- propertyInfo[_tokenId].rentAmount)}(''); //ig the address returned by ownerOf is already payable
+		require(success, 'Transaction failed');
+		(bool success1, ) = propertyOwner.call{value: (amount- propertyInfo[_tokenId].securityDeposit)}(''); //sending rent amount to property owner
+		require(success1, 'Transaction failed1');
+        tenentOf[_tokenId]=msg.sender;
+		//emit RentEvent(amount);
+	}
+    function payRent(uint256 _tokenId)external payable{
+        require(msg.sender==tenentOf[_tokenId],"");
+        uint256 amount = msg.value;
+        require(amount >=propertyInfo[_tokenId].rentAmount,"Not enough money sent");
+        (bool success, ) = ownerOf(_tokenId).call{value: amount}('');
+        require(success, 'Transaction failed');
+    }
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
     function totalSupply()public view returns(uint256){
             return _tokenIdCounter.current();
     }
-
+    function endRent(uint256 _tokenId)external{
+        require(msg.sender== ownerOf(_tokenId),"function only callable by owner of this property");
+        payable(tenentOf[_tokenId]).transfer(propertyInfo[_tokenId].securityDeposit);
+        tenentOf[_tokenId]=address(0);
+    }
+    receive() external payable {} // Function to receive Ether. msg.data must be empty
+    fallback() external payable {}// Fallback function is called when msg.data is not empty
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -13,26 +13,28 @@ module.exports = {
     localhost: {
       chainId: 31337,
     },
-    ropsten: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
+    // ropsten: {
+    //   url: API_URL,
+    //   accounts: [`0x${PRIVATE_KEY}`],
+    // },
   },
 };
 /////////////////////Custom tasks///////////////////
 task('mint','This mints an nft in the deployed contract ')
-.addOptionalParam("tokenuri","The greeting to print", "Hello, World!")
-.setAction(async({tokenuri})=>{
+.addParam("tokenuri",)
+.addParam("securitydeposit",)
+.addParam("rentamount",)
+.setAction(async({tokenuri,securitydeposit,rentamount})=>{
   const PropertyNft = await ethers.getContractFactory("PropertyNft")
   const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
   const signer =await ethers.getSigners();
   console.log(signer[0])
-  const newNft=await propertynft.connect(signer[0]).mint("0x70997970C51812dc3A010C7d01b50e0d17dc79C8",tokenuri)
+  const newNft=await propertynft.connect(signer[0]).mint("0x70997970C51812dc3A010C7d01b50e0d17dc79C8",tokenuri,securitydeposit,rentamount)
   console.log(newNft)
     
 })
 task('getTokenUri','Get token owner')
-.addOptionalParam("tokenid")
+.addParam("tokenid")
 .setAction(async({tokenid})=>{
   const PropertyNft = await ethers.getContractFactory("PropertyNft")
   const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
@@ -41,4 +43,56 @@ task('getTokenUri','Get token owner')
   console.log(tokenUri)
   console.log(`Owner of token ${tokenid} with token URI "${tokenUri}" is ${owner}`)
   
+})
+task('getBalance','Get the contract balance') //accepts no parameter
+.setAction(async()=>{
+  const PropertyNft = await ethers.getContractFactory("PropertyNft")
+  const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+  const balance=await propertynft.getBalance()
+  console.log(balance)
+})
+task('tenentOf','know who rents a property') 
+.addParam("tokenid")
+.setAction(async({tokenid})=>{
+  const PropertyNft = await ethers.getContractFactory("PropertyNft")
+  const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+  const tenent=await propertynft.tenentOf(tokenid)
+  console.log(`tenent of token ID ${tokenid} is ${tenent}`)
+})
+task('rent','Enter rent agreement') //accepts no parameter
+.addParam("tokenid")
+.setAction(async({tokenid})=>{
+  const PropertyNft = await ethers.getContractFactory("PropertyNft")
+  const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+  const signer =await ethers.getSigners();
+  const rent=await propertynft.connect(signer[0]).rent(tokenid,{value: ethers.utils.parseEther("5")})
+  console.log(rent)
+  console.log(`tenent of token ID ${tokenid} is ${rent}`)
+})
+task('payRent','Pay rent') //accepts no parameter
+.addParam("tokenid")
+.setAction(async({tokenid})=>{
+  const PropertyNft = await ethers.getContractFactory("PropertyNft")
+  const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+  const signer =await ethers.getSigners();
+  const paidRent=await propertynft.connect(signer[0]).payRent(tokenid,{value: ethers.utils.parseEther("2")})
+  console.log(paidRent)
+  console.log(`tenent has paid rent of token ID ${tokenid} with ${paidRent}`)
+})
+task('endRent','Close contract agreement') //accepts no parameter
+.addParam("tokenid")
+.addParam("owner")
+.setAction(async({tokenid,owner})=>{
+  const PropertyNft = await ethers.getContractFactory("PropertyNft")
+  const propertynft = await PropertyNft.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+  const signer =await ethers.getSigners();
+  console.log(signer[0].address)
+  for(i=0;i<20;++i){
+    if(signer[i].address==owner){
+      owner=signer[i];            //getting signer object for the owner account as owner variable only had an address in it previously
+    }
+  }
+  const endedRent=await propertynft.connect(owner).endRent(tokenid)
+  console.log(endedRent)
+  //console.log(`tenent has paid rent of token ID ${tokenid} with ${paidRent}`)
 })
